83eeb5ddc8d1f4f17ef92bf6788024fa6bd31a78
Austin Flores <ctrl4ltdeleteme@gmail.com>
2020-05-27 21:34:43 +0200

    First!

---
+++ persistance/csv.rb
@@ -0,0 +1,44 @@
+require 'csv'
+
+module Persistance
+  class Csv
+
+    attr_accessor :storage_name
+
+    CSV_PATH = File.join(
+      File.expand_path(__dir__),
+      '../data/__storage_name__.csv'
+    ).freeze
+
+    def initialize(storage_name)
+      @storage_name = storage_name
+    end
+
+    def save(data)
+      if data[:headers].nil?
+        CSV.open(file_path, 'wb') do |csv|
+          csv << data.keys
+          csv << data.values
+        end
+      else
+
+        CSV.open(file_path, 'wb') do |csv|
+          headers = data.delete(:headers)
+          headers.unshift('')
+          csv << headers
+
+          data.each do |key, values|
+            csv << values.unshift(key)
+          end
+
+        end
+      end
+    end
+
+    private
+
+    def file_path
+      CSV_PATH.gsub('__storage_name__', storage_name)
+    end
+  end
+end
---
+++ project_loc.rb
@@ -0,0 +1,57 @@
+if `git rev-parse --abbrev-ref HEAD`.chomp != 'develop'
+  raise 'Your branch is not develop. Only do this on develop'
+end
+
+$LOAD_PATH << File.expand_path(__dir__)
+require 'persistance/csv'
+require 'byebug'
+
+storage = Persistance::Csv.new('ruby_LOC')
+
+commits = `git log --pretty=oneline --no-merges| awk -F" " '{print $1}'`.split("\n")
+
+DATE_REGEX = /[0-9]{4}-[0-9]{2}-[0-9]{2}/.freeze
+
+project_lines = {}
+commits_backward = 0
+
+loc_check = {
+  ruby: "find #{Dir.pwd} -name '*.rb' | xargs wc -l",
+  typescript: "find #{Dir.pwd}/app/javascript -name '*.ts*' | xargs wc -l", # ts* to also catch tsx
+  javascript: "find #{Dir.pwd}/app/javascript -name '*.js*' | xargs wc -l", # js* to also catch jsx
+  coffee: "find #{Dir.pwd}/app/assets/ -name '*.coffee*' | xargs wc -l" # coffee* <- to also catch erb
+}
+
+rows = {
+  headers: [],
+  ruby: [],
+  typescript: [],
+  javascript: [],
+  coffee: []
+}
+
+
+
+begin
+  while commits_backward <= commits.length
+    `git checkout #{commits[commits_backward]}`
+
+    loc_check.each { |key, finder|
+      # wc -l returns multiple totals sometimes, ugh
+      rows[key] << `#{finder}`.split("\n").filter{|count_line| count_line[/ total/] }.map{|total_line| total_line[/[0-9]+/].to_i}.sum
+    }
+    rows[:headers] << `git show -q --pretty="format:%ai"`[DATE_REGEX]
+
+    commits_backward += 50
+  end
+
+  storage.save(rows)
+
+  `git checkout develop`
+rescue Exception => e
+
+  puts e.message
+  puts e.backtrace
+  puts "Failed hardcore, going back to develop branch"
+  `git checkout develop`
+end
